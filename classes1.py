fc3311d6552ce12576d88dc6b14c87f317ca1e0f
JuanCuartasCasas <jucuartasc@unal.edu.co>
2025-09-12 20:51:00 -0500

    Inicio class Encuesta

--- classes1.py
+++ classes1.py
@@ -1,1 +1,40 @@
-#Autores:Juan Diego Cuartas Casas Y 
+#Autores:Juan Diego Cuartas Casas Y Santiago Lopez Murcia
+#Motivo: Encuesta Proyectos
+
+def encuesta():
+    class Encuesta:
+        def __init__(self):
+            
+            self.preguntas = [
+                 "¿Considera que trabaja bien en equipo?",
+                 "¿Le gusta trabajar en equipo?"
+                 "¿Que idea de proyecto planea realizar?"
+
+
+
+
+
+
+
+        ]
+            self.respuestas = {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def main():
+    encuesta()
+
+if __name__ == "__main__":
+    main()
